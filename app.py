bf1c13e310568e729bcb46056b99191bc74bada5
vidya1899 <gvidya1899@gmail.com>
2021-01-22 23:16:10 +0530

    wip: changes in personality and basic code restructures

--- app.py
+++ app.py
@@ -2,8 +2,8 @@ from flask import Flask, request, render_template, flash, redirect
 import os
 import uuid
 
-from src.detector import runInference
-
+from src.dyslexiaCheck import runInference as dyslexia
+from src.personalityCheck import runInference as personality
 # set the project root directory as the static folder, you can set others.
 app = Flask(__name__, static_url_path='')
 
@@ -17,20 +17,33 @@ def application():
     #return app.send_static_file('index.html')
     return render_template('home.html')
 
-@app.route("/upload-image", methods=["GET", "POST"])
-def upload_image():
+@app.route("/upload-image-dyslexia", methods=["GET", "POST"])
+def upload_image_dyslexia():
     if request.method == "POST":
         if request.files:
             image = request.files["image"]
-            imageDirectory = os.path.join(os.getcwd(),"uploads")
+            imageDirectory = os.path.join(os.getcwd(),"uploads/dyslexia")
             uniqueFilename = str(uuid.uuid4())+"."+image.filename.split('.')[-1]
             imageDirectory = os.path.join(imageDirectory, uniqueFilename)
             image.save(imageDirectory)
             print("Image saved")
             #return redirect(request.url)
-    response = runInference(uniqueFilename)
-    return render_template("success.html", pyArgs = response)
+    response = dyslexia(uniqueFilename)
+    return render_template("dyslexia-report.html", pyArgs = response)
 
+@app.route("/upload-image-personality", methods=["GET", "POST"])
+def upload_image_personality():
+    if request.method == "POST":
+        if request.files:
+            image = request.files["image"]
+            imageDirectory = os.path.join(os.getcwd(),"uploads/personality")
+            uniqueFilename = str(uuid.uuid4())+"."+image.filename.split('.')[-1]
+            imageDirectory = os.path.join(imageDirectory, uniqueFilename)
+            image.save(imageDirectory)
+            print("Image saved")
+            #return redirect(request.url)
+    response = personality(uniqueFilename)
+    return render_template("personality-report.html", pyArgs = response)
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0',port='80')
--- src/personalityCheck.py
+++ src/personalityCheck.py
@@ -1,5 +1,4 @@
 import sys
-sys.path.insert(0, '/Users/yashbhat/GraphForLab')
 
 import cv2
 import sys
@@ -8,17 +7,10 @@ import sys
 import pandas as pd
 import numpy as np
 import shutil
-#import seaborn as sns
-#import matplotlib.pyplot as plt
 from statistics import mean, mode
 from word_extract import extract_words
 from baseline2 import baseline_extract
 from baseline2 import get_size
-# import statistics as stat
-#from click_and_crop import get_ts
-#from click_and_crop import get_hw
-#from opencv_text_detection.text_detection import textdetection
-from tbar import tbar_length
 from hori import get_hori
 from hori import get_f2
 import time
@@ -30,67 +22,11 @@ def display_images(im):
 	cv2.imshow("Image",im)
 	cv2.waitKey(0)
 
-def main(filename):
-	# import picamera
-
-	# with picamera.PiCamera() as camera:
-	#     camera.resolution = (1024, 768)
-	#     camera.start_preview()
-	#     # Camera warm-up time
-	#     time.sleep(10)
-	#     camera.capture('foo.jpg')
-
-	# foo = cv2.imread('foo.jpg')
-	# get_hw(foo)
-
-	# sample = cv2.imread('./HW_118_dataset/dataset-page-008.jpg')
-	# sample = cv2.imread(sys.argv[1])
-	#rpath = '/Users/yashbhat/Graphology/api/'
-	# sample = cv2.imread(filename)
+def runInference(filename):
 	sample = cv2.imread('uploads/'+filename)
-	# height,width = sample.shape[0],sample.shape[1]
-	# print("h,w:",height,width)
-	# sample = cv2.resize(sample, dsize=(500, int(500 * height / width)), interpolation=cv2.INTER_AREA)
-
-	# cv2.imshow('sample',sample)
-	# cv2.waitKey(0)
-
 	print("Handwriting Analysis...")
-	#inp = input("Enter the /path/to/the image containing the handwriting:\n")
-	#inputs = [cv2.imread(file) for file in glob.glob("../HW_118_dataset/dataset-page-008.jpg")]
-
-	#img = cv2.imread(inp)
-
-	#extract words
-
-	#METHOD 1 : based on contour detection
-	#extract_words(img)
-
-	#METHOD 2: based on fast ease DL pre trained text detector
-	#textdetection(input)
-	#for input in inputs:
-
-	start_time = time.time()
 	extract_words(sample)
-	#break
-
-	# cv2.destroyAllWindows()
-
-	# print("Load words to start analysing...")
-
-
-	#read all words extracted from results
-	# words = [cv2.imread(file) for file in glob.glob("./words/*")]
 	words = [cv2.imread(file) for file in glob.glob('samples/*')]
-
-	# print("Select ts:")
-	#words_t = [cv2.imread(file) for file in glob.glob("./words/*")]
-
-	# for i,word_t in enumerate(words):
-	# 	#t_present = False
-	# 	#get_ts(word_t,i,t_present)
-	# 	get_ts(word_t,i)
-
 	console_display = []
 	sizes = []
 
@@ -98,34 +34,10 @@ def main(filename):
 		#find baseline
 		try:
 			baseline_img,f1_size,height = baseline_extract(word)
-			# display_images(baseline_img)
-			#print("word shape:",word.shape)
-			sizes.append(get_size(f1_size,height))
-			#sizes.append(f1_size)
-			#heights.append(height)
-			#temp = (f1_size*100)/height
-			#p.append(temp)
 		except:
 			print("no size for this")
-	# print("FEATURE 1: SIZE and SLANT : Level of Socially Outgoing.")
-
-	# console_display.append("FEATURE 1: SIZE and SLANT : Level of Socially Outgoing.")
-
-	# print('wtyfwagdsize',sizes)
-	# print(sizes)
-	# try:
-	# 	print("Size:",mode(sizes))
-	# 	console_display.append(str(mode(sizes)))
-	# except:
-	# 	print("Size:",sizes[0])
-	# 	console_display.append(str(sizes[0]))
-
 	map_size = [0,0,0]
 	get_the_size = {0:'medium',1:'high',2:'low'}
-
-	# sizes = ['high','low','high','medium','low']
-
-	#map_size -> [medium, high and low]
 	for size in sizes:
 		if size == "high":
 			# map_size['high'] += 1
@@ -262,36 +174,6 @@ def main(filename):
 	# t bar - length - enthusiasm
 	###FIVE###
 	console_display.append(f5)
-
-	#
-	#
-	# if (f6 == 99):
-	# 	print('High')
-	# 	# console_display.append('High degree of tolerance')
-	# else:
-	# 	print('Limited')
-	# 	# console_display.append('Limited degree of tolerance')
-	#
-	# print("FEATURE 3: Placement of t-bar : Practicality.")
-	# # console_display.append("FEATURE 3: Placement of t-bar : Practicality.")
-	# print(f3)
-	# console_display.append(str(f3))
-	#
-	# print("FEATURE 4: The slant of the t-bar : Optimism.")
-	# # console_display.append("FEATURE 4: The slant of the t-bar : Optimism.")
-	# print(optimism[f6])
-	# console_display.append(str(optimism[f6]))
-	#
-	# print("FEATURE 5: Length of t-bar : Enthusiasm.")
-	# print(f5)
-	# # console_display.append("FEATURE 5: Length of t-bar : Enthusiasm.")
-	# console_display.append(str(f5))
-
-	# print("total time ---- %s ----",time.time()-start_time)
 	#console_display.append("total time ---- %s ----" + str(time.time()-start_time))
 	return console_display
 
-
-if __name__ == "__main__":
-	res = main(sys.argv[1])
-	print(res)
