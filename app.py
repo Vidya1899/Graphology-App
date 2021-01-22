fb159632c9db2c9b8d0a7269cf65ce2d28e20921
vidya1899 <gvidya1899@gmail.com>
2021-01-23 00:25:33 +0530

    error handling

--- app.py
+++ app.py
@@ -19,31 +19,38 @@ def application():
 
 @app.route("/upload-image-dyslexia", methods=["GET", "POST"])
 def upload_image_dyslexia():
-    if request.method == "POST":
-        if request.files:
-            image = request.files["image"]
-            imageDirectory = os.path.join(os.getcwd(),"uploads/dyslexia")
-            uniqueFilename = str(uuid.uuid4())+"."+image.filename.split('.')[-1]
-            imageDirectory = os.path.join(imageDirectory, uniqueFilename)
-            image.save(imageDirectory)
-            print("Image saved")
-            #return redirect(request.url)
-    response = dyslexia(imageDirectory)
-    return render_template("dyslexia-report.html", pyArgs = response)
+    try:
+        if request.method == "POST":
+            if request.files:
+                image = request.files["image"]
+                imageDirectory = os.path.join(os.getcwd(),"uploads/dyslexia")
+                uniqueFilename = str(uuid.uuid4())+"."+image.filename.split('.')[-1]
+                imageDirectory = os.path.join(imageDirectory, uniqueFilename)
+                image.save(imageDirectory)
+                print("Image saved")
+                #return redirect(request.url)
+        response = dyslexia(imageDirectory)
+        return render_template("dyslexia-report.html", pyArgs = response)
+    except:
+        return render_template("error.html")
+
 
 @app.route("/upload-image-personality", methods=["GET", "POST"])
 def upload_image_personality():
-    if request.method == "POST":
-        if request.files:
-            image = request.files["image"]
-            imageDirectory = os.path.join(os.getcwd(),"uploads/personality")
-            uniqueFilename = str(uuid.uuid4())+"."+image.filename.split('.')[-1]
-            imageDirectory = os.path.join(imageDirectory, uniqueFilename)
-            image.save(imageDirectory)
-            print("Image saved")
-            #return redirect(request.url)
-    response = personality(imageDirectory)
-    return render_template("personality-report.html", pyArgs = response)
+    try:
+        if request.method == "POST":
+            if request.files:
+                image = request.files["image"]
+                imageDirectory = os.path.join(os.getcwd(),"uploads/personality")
+                uniqueFilename = str(uuid.uuid4())+"."+image.filename.split('.')[-1]
+                imageDirectory = os.path.join(imageDirectory, uniqueFilename)
+                image.save(imageDirectory)
+                print("Image saved")
+                #return redirect(request.url)
+        response = personality(imageDirectory)
+        return render_template("personality-report.html", pyArgs = response)
+    except:
+        return render_template("error.html")
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0',port='80')
